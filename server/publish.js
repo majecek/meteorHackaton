fe02851464dd19d8ff777f3452d56f3b2b29b273
Marek Chowaniok <marek.chowaniok@gmail.com>
2013-04-20 20:50:17 +0200

    Twitterbootstrap change

--- server/publish.js
+++ server/publish.js
@@ -9,49 +9,49 @@ Meteor.publish("allGames", function() {
 Meteor.startup(function() {
 	// code to run on server at startup
 	Games.remove({});
-	if (Games.find().count() === 0) {
-		Games.insert({
-			name : "game1",
-			status : "WAITING",
-			size : {
-				x : 5,
-				y : 5
-			},
-			players : [ {
-				email : "Adam@example.com",
-				symbol : "x"
-			} ],
-			board : [ {
-				x : 1,
-				y : 1,
-				moveNumber : 1,
-				email : "Adam@example.com"
-			} ],
-            nextPlayer: ""
-		});
-		Games.insert({
-			name : "game2",
-			status : "ACTIVE",
-			size : {
-				x : 5,
-				y : 5
-			},
-			players : [ {
-				email : "Dan@example.com",
-				symbol : "x"
-			}, {
-				email : "Emil@example.com",
-				symbol : "o"
-			} ],
-			board : [ {
-				x : 1,
-				y : 1,
-				moveNumber : 1,
-				email : "Emil@example.com"
-			} ],
-            nextPlayer: ""
-		});
-	}
+//	if (Games.find().count() === 0) {
+//		Games.insert({
+//			name : "game1",
+//			status : "WAITING",
+//			size : {
+//				x : 5,
+//				y : 5
+//			},
+//			players : [ {
+//				email : "Adam@example.com",
+//				symbol : "x"
+//			} ],
+//			board : [ {
+//				x : 1,
+//				y : 1,
+//				moveNumber : 1,
+//				email : "Adam@example.com"
+//			} ],
+//            nextPlayer: ""
+//		});
+//		Games.insert({
+//			name : "game2",
+//			status : "ACTIVE",
+//			size : {
+//				x : 5,
+//				y : 5
+//			},
+//			players : [ {
+//				email : "Dan@example.com",
+//				symbol : "x"
+//			}, {
+//				email : "Emil@example.com",
+//				symbol : "o"
+//			} ],
+//			board : [ {
+//				x : 1,
+//				y : 1,
+//				moveNumber : 1,
+//				email : "Emil@example.com"
+//			} ],
+//            nextPlayer: ""
+//		});
+//	}
 
 	Meteor.methods({
 
