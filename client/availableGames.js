a277d8084cbce4dc8ffce446545b2ac214394ece
mhynar <mhynar@vendavo.com>
2013-04-20 16:14:42 +0200

    handling with games

--- client/availableGames.js
+++ client/availableGames.js
@@ -1,12 +1,29 @@
-Template.availableGames.games = function () {
-    return Games.find({});
+Template.availableGames.games = function() {
+	return Games.find({});
 };
 
+Template.availableGames.emails = function() {
+	var p1 = this.players[0];
+	var p2 = this.players[1];
+	if (p2 != null) {
+		return "(" + p1.email + " / " + p2.email + ")";
+	}
+	return p1.email;
+}
+
+Template.availableGames.canJoin = function() {
+	var p1 = this.players[0];
+	var p2 = this.players[1];
+	if (p2 != null) {
+		return false;
+	}
+	return true;
+}
 
 Template.availableGames.events({
 	'click input.join-game' : function() {
 
-        Session.set('game', this);
+		Session.set('game', this);
 
 		Games.update({
 			_id : this['_id']
@@ -17,36 +34,22 @@ Template.availableGames.events({
 		});
 	},
 	'click input.create-game' : function() {
-
-        var game = {
-            status: "WAITING",
-            size: {
-                x: 5,
-                y: 5
-            },
-            players: [
-                {
-                    name: "Adam",
-                    symbol: "x"
-                },
-                {
-                    name: "Bohumil",
-                    symbol: "o"
-                }
-            ],
-            board: [
-                {
-                    x: 1,
-                    y: 1,
-                    moveNumber: 1,
-                    name: "Adam"
-                }
-            ]
-        };
+		var userEmail = Meteor.user().emails[0].address;
+		var game = {
+			status : "WAITING",
+			size : {
+				x : 5,
+				y : 5
+			},
+			players : [ {
+				email : userEmail,
+				symbol : "x"
+			} ]
+		};
 
 		var id = Games.insert(game);
-        game['_id'] = id;
+		game['_id'] = id;
 
-        Session.set('game', game);
+		Session.set('game', game);
 	}
 });
--- server/publish.js
+++ server/publish.js
@@ -1,90 +1,95 @@
-
 Meteor.publish("allGames", function() {
 	return Games.find();
-//	return Games.find({	status: "ACTIVE"});
+	// return Games.find({ status: "ACTIVE"});
 });
-
-
 
 Meteor.startup(function() {
 	// code to run on server at startup
 	Games.remove({});
 	if (Games.find().count() === 0) {
 		Games.insert({
-            name: "game1",
-			status: "ACTIVE",
-			size : {x: 5, y: 5 },
+			name : "game1",
+			status : "WAITING",
+			size : {
+				x : 5,
+				y : 5
+			},
 			players : [ {
-				name : "Adam",
+				email : "Adam@example.com",
 				symbol : "x"
-			}, {
-				name : "Bohumil",
-				symbol : "o"
 			} ],
 			board : [ {
 				x : 1,
 				y : 1,
 				moveNumber : 1,
-				name : "Adam"
+				email : "Adam@example.com"
 			} ]
 		});
 		Games.insert({
-            name: "game2",
-			status: "INACTIVE",
-			size : {x: 5, y: 5 },
+			name : "game2",
+			status : "ACTIVE",
+			size : {
+				x : 5,
+				y : 5
+			},
 			players : [ {
-				name : "Dan",
+				email : "Dan@example.com",
 				symbol : "x"
 			}, {
-				name : "Emil",
+				email : "Emil@example.com",
 				symbol : "o"
 			} ],
 			board : [ {
 				x : 1,
 				y : 1,
 				moveNumber : 1,
-				name : "Adam"
+				email : "Emil@example.com"
 			} ]
 		});
 	}
 
+	Meteor.methods({
 
-    Meteor.methods({
+		findGameByID : function(gameID) {
+			var game = Games.find({
+				_id : gameID
+			}).fetch();
+			return game;
+		},
 
-        findGameByID: function(gameID) {
-            var game = Games.find({_id: gameID}).fetch();
-            return game;
-        },
+		findGameByName : function(gameName) {
+			var game = Games.findOne({
+				name : gameName
+			});
+			return game;
+		},
 
-        findGameByName: function(gameName) {
-            var game = Games.findOne({name: gameName});
-            return game;
-        },
+		createGame : function() {
+			Games.insert({
+				name : "game3",
+				status : "ACTIVE",
+				size : [ 5, 5 ],
+				players : [ {
+					name : "Dan",
+					symbol : "x"
+				}, {
+					name : "Emil",
+					symbol : "o"
+				} ],
+				board : [ {
+					x : 1,
+					y : 1,
+					moveNumber : 1,
+					name : "Adam"
+				} ]
+			});
+		},
 
-        createGame: function() {
-            Games.insert({
-                name: "game3",
-                status: "ACTIVE",
-                size : [ 5, 5 ],
-                players : [ {
-                    name : "Dan",
-                    symbol : "x"
-                }, {
-                    name : "Emil",
-                    symbol : "o"
-                } ],
-                board : [ {
-                    x : 1,
-                    y : 1,
-                    moveNumber : 1,
-                    name : "Adam"
-                } ]
-            });
-        },
-
-        cancelGame: function(gameId) {
-            Games.remove({_id: gameId});
-        }
-    });
+		cancelGame : function(gameId) {
+			Games.remove({
+				_id : gameId
+			});
+		}
+	});
 
 });
